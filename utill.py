49633858c1f2ab36bc40c237309d68554f989d28
Eyob Tadele-Manhardt <eyobtade@buffalo.edu>
2022-11-17 22:47:28 -0500

    Implemented get_zipfile_information function
    Used Selenium instead of bs4 to scrape content from webpage since the
    zipfile information we're interested in was dynamically generated with
    Java Script.

--- utill.py
+++ utill.py
@@ -3,6 +3,15 @@ In this file we - we are going to create helper functions that will help us scra
 """
 
 # imports 
+from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.common.by import By
+from typing import List
+from time import sleep
+from datetime import datetime
+import pandas as pd
+import urllib.request
+import zipfile
+import os
 
 URL : str  = "https://divvy-tripdata.s3.amazonaws.com/index.html"
 
@@ -18,15 +27,69 @@ URL : str  = "https://divvy-tripdata.s3.amazonaws.com/index.html"
 # We want to extract all of the information 
 
 def get_zipfile_information(url: str, destination: str) -> None:
-  """Extracts and saves the zipfile name, last modified date, and zipfile size information from the 
-  table located on the url page as a csv file at destination location.
+  """Extracts and saves the name of the zip file, the last date it was modified, and the file size from the 
+  table located on the url page. Saves the extracted content as a csv file in destination path.
 
   args:
     url (str): link to the webpage where the content is stored in html and java script format.
     destination (str): Path to the location where to store data.
   """
-  pass
+  # Don't launch browser to webpage
+  options = ChromeOptions()
+  options.headless = True
 
+  # Set up driver 
+  driver = Chrome(options=options)
+  # Fetch webpage 
+  driver.get(url)
+
+  zipfile_info_data : dict = {"filename": [], "last modified date": [], "filesize": []}
+  table_id : str = "tbody-content"
+  table_data = driver.find_element(By.ID, table_id)
+
+  sleep(3)
+
+  table_content : List[str] = table_data.text.split("\n")
+
+  for index, row in enumerate(table_content):
+    row_content : List[str] = row.split(" ")
+    filename : str = row_content[0]
+
+    valid_row : bool = filename[:5].isdigit()
+
+    if valid_row:
+      print(f"row {index + 1} is valid! Extracting content ...")
+      # Extract date information
+      month = row_content[1]
+      day = row_content[2][:-2] # Strip the last 2 elements i.e 2nd, 5th, 18th
+      year = row_content[3][:-1] # Remove comma at end of text
+
+      # Extract time information
+      hour_minute = ":".join(row_content[4].split(":")[:-1])
+      pm_am = row_content[5].upper()
+      time = "".join([hour_minute, pm_am])
+
+      # Combine date time information as a dt object
+      last_modified_date = datetime.strptime(" ".join([month, day, year, time]), "%b %d %Y %I:%M%p")
+
+      # File information
+      file_size = " ".join(row_content[6:8])
+
+      # Add row content to dictionary 
+      zipfile_info_data["filename"].append(filename)
+      zipfile_info_data["last modified date"].append(last_modified_date)
+      zipfile_info_data["filesize"].append(file_size)
+
+      print(f"row {index + 1} extracted")
+
+    else:
+      continue
+
+  data : pd.DataFrame = pd.DataFrame.from_dict(zipfile_info_data)
+
+  # Save extracted data as a csv file
+  csv_filename : str = f"{destination}/zipfile_info.csv"
+  data.to_csv(csv_filename, index = False)
 
 def extract_zipfile_to(destination: str) -> None:
   """Downloads and unzips the zip file content to destination
