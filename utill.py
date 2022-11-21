5fedd7579653425e7a78af056557ac7055344756
Eyob Tadele-Manhardt <eyobtade@buffalo.edu>
2022-11-20 20:43:57 -0500

    Added Default parameters to extract_zipfile + more
    Implemented a get_path helper function that creates paths to the correct
    Destinations and  is independent to the operating systems design (i.e don’t need to worry / vs //).

--- utill.py
+++ utill.py
@@ -13,6 +13,7 @@ import urllib.request
 import zipfile
 import os
 import shutil
+from pathlib import Path
 
 # Idea 
 # Extract information from the URL
@@ -25,23 +26,26 @@ import shutil
 
 # We want to extract all of the information 
 
-def extract_zipfile_metadata(url: str = "", destination: str = "", metadata_filename : str = "zipfile_metadata.csv" ) -> None:
+DIRECTORY : str = Path.cwd()
+
+def extract_zipfile_metadata(data_directory: str, metadata_filename : str, url: str = "https://divvy-tripdata.s3.amazonaws.com/index.html") -> None:
   """Extracts and saves the name of the zip file, the last date it was modified, and the file size from the 
-  table located on the url page. Saves the extracted content as a csv file in destination path.
+  table located on the url page. Saves the extracted content as a csv file in data_directory path.
 
   args:
     url (str): link to the webpage where the content is stored in html and java script format.
-    destination (str): Path to the location where to store data.
+    data_directory (str): Path to the location where to store data.
   """
   webpage_content : str = get_webpage_content(url)
   zipfile_metadata : dict = webpage_content_to_dict(webpage_content)
-  files_in_directory : List[str] = [file for file in os.listdir(destination) if file[-4:] == ".csv"]
+  path : str = os.path.join(DIRECTORY, data_directory)
+  files_in_directory : List[str] = [file for file in os.listdir(path) if file[-4:] == ".csv"]
 
   if metadata_filename in files_in_directory:
-    update_metadata(zipfile_metadata, destination, metadata_filename)
+    update_metadata(zipfile_metadata, data_directory, metadata_filename)
 
   else:
-    save_metadata(zipfile_metadata, destination, metadata_filename)
+    save_metadata(zipfile_metadata, data_directory, metadata_filename)
 
 
 def get_webpage_content(url: str) -> str:
@@ -57,7 +61,9 @@ def get_webpage_content(url: str) -> str:
 
 def webpage_content_to_dict(webpage_content: str) -> dict:
   data : dict = {"filename": [], "last_modified_date": [], "filesize": []}
-  table_content : List[str] = webpage_content.split(" ")
+  table_content : List[str] = webpage_content.split("\n")
+  # print(table_content)
+  # print(table_content)
 
   for row in table_content:
     if row[:6].isdigit():
@@ -68,16 +74,15 @@ def webpage_content_to_dict(webpage_content: str) -> dict:
 
   return data 
 
-def save_metadata(data : dict, destination: str, filename : str) -> None:
-  filepath : str = f"{destination}/{filename}"
-  pd.DataFrame.from_dict(data).to_csv(filepath, index= False)
+def save_metadata(data : dict, data_directory: str, filename : str) -> None:
+  pd.DataFrame.from_dict(data).to_csv(get_path(data_directory, filename), index= False)
 
-def update_metadata(data : dict, destination : str, filename: str) -> None:
-  filepath : str = f"{destination}/{filename}"
+def update_metadata(data : dict, data_directory : str, filename: str) -> None: 
+  filepath : str = get_path(data_directory, filename)
   filenames, modified_dates, filesize = list(data.values())
   rows = zip(filenames, modified_dates, filesize)
 
-  zipfile_metadata : pd.DataFrame = pd.read_csv(filepath, parse_date = ["last_modified_date"])
+  zipfile_metadata : pd.DataFrame = pd.read_csv(filepath, parse_dates = ["last_modified_date"])
 
   for row in rows:
     name, date, size = row
@@ -89,7 +94,7 @@ def update_metadata(data : dict, destination : str, filename: str) -> None:
     else:
       zipfile_metadata.loc[len(zipfile_metadata)] = [name, date, size]
 
-  save_metadata(zipfile_metadata.to_dict(orient="list"), destination, filename)
+  save_metadata(zipfile_metadata.to_dict(orient="list"), data_directory, filename)
 
 
 def extract_row_content(row : str) -> tuple[str, datetime, str]:
@@ -110,42 +115,61 @@ def extract_row_content(row : str) -> tuple[str, datetime, str]:
 
   return filename, last_modified_date, file_size
 
-def archive_data(destination : str, filename: str) -> None:
-  year : str = filename[:5]
-  from_path : str = f"{destination}/{year}/{filename}"
-  archive_path : str = f"{destination}/archive"
+def archive_data(data_directory : str, filename: str) -> None:
+  year : str = filename[:4]
+  from_path : str = get_path(data_directory, year, filename)
+  archive_path : str = get_path(data_directory, "archive")
   files_in_archive : List[str] = [file for file in os.listdir(archive_path) if file[:-4] == ".csv"]
   if filename in files_in_archive:
     j = len(filename[:-4]) # extracts name part of the filename
     version_num = len([f for f in files_in_archive if filename[:j] == f[:j]])
     new_filename = f"{filename[:j]}_{version_num + 1}.csv"
-    to_path = f"{archive_data}/{new_filename}"
+    to_path = get_path(archive_path, new_filename, include_dir=False)
     shutil.make_move(from_path, to_path)
   
   else:
-    to_path : str = f"{archive_path}/{filename}"
+    to_path : str = get_path(archive_path, filename, include_dir=False)
     shutil.move(from_path, to_path)
 
-def extract_zipfile(url: str, destination: str = "", metadata_filename : str = "") -> None:
-  """Downloads and unzips the zip file content to destination
+def extract_zipfile(url: str = "https://divvy-tripdata.s3.amazonaws.com", data_directory: str = "data", destination : str = "raw", metadata_filename : str = "zipfile_metadata.csv") -> None:
+  """Downloads and unzips the zip file content to data_directory (i.e data/raw) folder organized by the year
+  the data was collected
 
   Args:
-      destination (str): Path to the location where to store data. 
+      data_directory (str): Path to the location where to store data. 
   """
-  extract_zipfile_metadata() 
-  filepath : str = f"{destination}/{metadata_filename}"
+  extract_zipfile_metadata(data_directory, metadata_filename) 
+  filepath : str = get_path(data_directory, metadata_filename)
   metadata : pd.DataFrame = pd.read_csv(filepath, parse_dates=["last_modified_date"])
 
   zipfile_filenames = list(metadata.filename)
-  source_and_filename = [(f"{url}/{filename}", filename) for filename in zipfile_filenames]
+  source_and_filename = [(f"{url}/{filename}", f"{filename[:-4]}.csv") for filename in zipfile_filenames]
 
   for source, filename in source_and_filename:
+    # print(source)
     file, _ = urllib.request.urlretrieve(source)
     with zipfile.ZipFile(file) as zipfile_content:
-      year : str = filename[:5]
-      path : str = f"{destination}/{year}"
-      if filename not in [f for f in os.listdir(path)]:
-        zipfile_content.extractall(path)
+      year : str = filename[:4]
+      path : str = get_path(data_directory, destination, year)
+      # if the path exists
+      if os.path.isdir(path):
+        # make sure sure file content does not exist
+        if filename not in [f for f in os.listdir(path)]:
+          print(f"Downloading {filename} to {path}")
+          zipfile_content.extractall(path)
+        else:
+          # skip file i.e file already exist 
+          print(f"{filename} already exists in {path}")
+      else:
+          print(f"Downloading {filename} to {path}")
+          zipfile_content.extractall(path)
+
+def get_path(*paths : tuple[str], include_dir : bool = True) -> str:
+  if include_dir:
+    return os.path.join(DIRECTORY, *paths)
+  else:
+    return os.path.join(*paths)
+
 
 
 
