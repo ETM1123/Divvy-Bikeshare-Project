8061674b2feae49477b5107b7e22b2896168c8d2
Eyob Tadele-Manhardt <eyobtade@buffalo.edu>
2022-11-23 23:02:39 -0500

    Add skeleton templates for Metadata clas

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -1,4 +1,6 @@
 """ This file is going to contain the refactored code from util.py"""
+from datetime import datetime
+import pandas as pd
 
 class Zipfile:
   """Downloads zipfile from web address and stores zipfile content locally"""
@@ -7,4 +9,34 @@ class Zipfile:
 
 class Metadata:
   """Extracts zipfile's metadata from web address and saves it as a csv file on local computer"""
-  pass
+  METADATA_URL : str = ""
+  METADATA_TABLE_ID : str = ""
+  Min_ROW_LENGTH : int = 67
+  def __inti__(self, directory : str, filename : str = "zipfile_metadata.csv"):
+    self.directory = directory
+    self.filename = filename
+
+  def extract(self):
+   raise NotImplementedError
+
+  def extract_row_content(self, row: str) -> tuple[str, datetime, str]:
+    raise NotImplementedError
+
+  def fetch_data(self) -> str:
+    raise NotImplementedError
+  
+  def get_data(self) -> pd.DataFrame:
+    raise NotImplementedError
+
+  def archive_data(self, from_full_path : str, to_dir: str, filename : str) -> None:
+    raise NotImplementedError
+
+  def update(self, data : dict[str, list[str]]) -> None:
+    raise NotImplementedError
+
+  def convert_to_dict(self, data : str):
+    raise NotImplementedError
+
+  def convert_to_csv(self, data : dict[str : list]) -> None: 
+    raise NotImplementedError
+    
---
+++ tests/test_extract_metadata.py
@@ -0,0 +1,21 @@
+"""This file test the methods from extract.Metadata module"""
+
+from extract.extract import Metadata
+
+def test_extract_row_content() -> None:
+  raise NotImplementedError
+
+def test_get_data() -> None:
+  raise NotImplementedError
+
+def test_archive_data() -> None:
+  raise NotImplementedError
+
+def test_update() -> None:
+  raise NotImplementedError
+
+def convert_to_dict() -> None:
+  raise NotImplementedError
+
+def convert_to_csv() -> None:
+  raise NotImplementedError
