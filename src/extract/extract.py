0e8820c0683693ac3b2861efbc78e4301a5c11ae
Eyob Tadele-Manhardt <eyobtade@buffalo.edu>
2022-11-23 22:05:21 -0500

    Set up config file and refactor util.py
    Start converting code in util.py into a package.

---
+++ src/extract/extract.py
@@ -0,0 +1,10 @@
+""" This file is going to contain the refactored code from util.py"""
+
+class Zipfile:
+  """Downloads zipfile from web address and stores zipfile content locally"""
+  pass
+
+
+class Metadata:
+  """Extracts zipfile's metadata from web address and saves it as a csv file on local computer"""
+  pass
