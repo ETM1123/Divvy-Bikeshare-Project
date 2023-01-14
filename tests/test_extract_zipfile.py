b1eb9c19fa178b1c94c6490053b5a89a594992a5
Eyob Tadele-Manhardt <eyobtade@buffalo.edu>
2023-01-14 17:48:21 -0500

    refactor[test]: Fixed download_zipfile bug

--- tests/test_extract_zipfile.py
+++ tests/test_extract_zipfile.py
@@ -1,7 +1,9 @@
 """This file tests the methods form extract.Zipfile module"""
 from extract.extract import Zipfile 
-from helpers.util import add_file, delete_file, get_path, test_data_directory
+from helpers.util import add_file, delete_file, get_path, create_zipfile, test_data_directory
 import pytest
+import zipfile
+import os
 zf = Zipfile()
 
 def test_get_source() -> None:
@@ -32,5 +34,28 @@ def test_file_exists() -> None:
     # clean up
     file_path = get_path(filename, include_home_dir=True)
     delete_file(file_path)
+    
+def test_download_zipfile() -> None:
+  # Set up:
+  zip_file_name = "files.zip"
+  zip_file_path = os.path.join(test_data_directory, zip_file_name)
 
+  dummy_file_name = "file1.txt" 
+  dummy_file_path : str = os.path.join(test_data_directory, dummy_file_name)
 
+  # Create compressed zipfile with dummy file in it
+  create_zipfile(zip_file_path, dummy_file_path)
+
+  files_in_test_dir_before = os.listdir(test_data_directory)
+
+  # Extract zipfile content 
+  # i.e new files should be added to dir
+  zf.download_zipfile(zip_file_path, dummy_file_name, test_data_directory)
+  # Get filenames from dir
+  files_in_test_dir_after = os.listdir(test_data_directory)
+  # check the if content from zipfile extracted properly
+  assert len(files_in_test_dir_after) > len(files_in_test_dir_before) and dummy_file_name in files_in_test_dir_after
+
+  # remove compressed and extracted file
+  os.remove(dummy_file_path)
+  os.remove(zip_file_path)
