4c1572d0fd25f945f17ea81bc84b699da06e3598
Eyob Tadele-Manhardt <eyobtade@buffalo.edu>
2023-01-28 16:22:02 -0500

    Preprocessing: add function to extact rides and station infromation
    This commit enhances the data processing capabilities of the application by
    introducing new functions to extract rides and station information from raw data.
    These functions will enable the system to extract relevant information
    from data in a structured and efficient manner. Specifically, the following
    functions have been added:
    
    get_rides_info
    get_station_info
    create_files
    update_station_id

--- src/transform/transform.py
+++ src/transform/transform.py
@@ -33,7 +33,6 @@ def remove_period(s: str) -> str:
     return s[:-2]
   return s
 
-
 def clean_station_id(df : pd.DataFrame, colname: str) -> pd.DataFrame:
   df[colname] = df[colname].apply(lambda station_id: remove_period(station_id))
   return df
@@ -43,13 +42,72 @@ def cast_values_to_str(df, *colnames) -> pd.DataFrame:
   df = df.astype(new_col_types)
   return df
 
-def change_column_name(df : pd.DataFrame, old_name : str, new_name : str) -> pd.DataFrame:
-  df = df.rename(columns={old_name:new_name})
+def change_column_name(df : pd.DataFrame, **col_name_mapping : dict[str, str]) -> pd.DataFrame:
+  df = df.rename(columns=col_name_mapping)
   return df
 
 def remove_null_values(df : pd.DataFrame) -> pd.DataFrame:
   df = df.dropna(axis=0, how='any', inplace=False)
   return df 
+
+def get_station_info(df : pd.DataFrame) -> pd.DataFrame:
+  start_station_cols = ['start_station_id', 'start_station_name', 'start_lat', 'start_lng']
+  end_station_cols = ['end' + colname[5:] for colname in start_station_cols]
+  new_col_names = ['station_id', 'station_name', 'latitude', 'longitude']
+  new_start_station_cols = {old_name: new_name for old_name, new_name in zip(start_station_cols, new_col_names)}
+  new_end_station_cols = {old_name: new_name for old_name, new_name in zip(end_station_cols, new_col_names)}
+
+
+  start_station_info = df.loc[:, start_station_cols].pipe(change_column_name, **new_start_station_cols)
+  end_station_info = df.loc[:, end_station_cols].pipe(change_column_name, **new_end_station_cols)
+
+  stations = (pd.concat([start_station_info, end_station_info])
+              .drop_duplicates(subset=['station_id', 'station_name'], keep='last')
+              .reset_index(drop=True))
+  return stations
+
+
+def create_files(master_station_filename : str, 
+                 station_mapping_filename : str, 
+                 biketrip_filename : str, 
+                 original_station_info : pd.DataFrame, 
+                 rides : pd.DataFrame) -> None:
+  """Creates three csv fils that tracks:
+      1. Original stations_id mapping with their corresponding information
+      2. New station_id mapping (that we generate) for unique station name
+      3. Unique bike trips with the new station_id mappings
+  """
+  stations = (original_station_info
+                  .loc[:, ['station_name', 'latitude', 'longitude']]
+                  .drop_duplicates(subset=['station_name'], keep='last')
+                  .reset_index(drop=True))
+  # Add new station_id mapping 
+  stations.insert(0, 'ID', [i+1 for i in range(stations.shape[0])])
+
+  # update rides_df with new station_id mapping 
+  master_rides_df = update_station_ids(rides, stations)
+
+  # save file in process directory
+  original_station_info.to_csv(master_station_filename, index=False)
+  stations.to_csv(station_mapping_filename, index=False)
+  master_rides_df.to_csv(biketrip_filename, index=False)
+
+def update_station_ids(rides, stations):
+  """Map the station names from rides df to their corresponding station_ids from stations df"""
+  df1 = (pd.merge(rides, stations, left_on='start_station_name', right_on='station_name')
+         .loc[:, ['ride_id', 'rideable_type', 'ID', 'started_at', 'ended_at', 'end_station_name']]
+         .rename(columns={'ID':'from_station_id'}))
+  
+  df2 = (pd.merge(df1, stations, left_on='end_station_name', right_on='station_name')
+         .loc[:, ['ride_id', 'rideable_type', 'from_station_id','ID', 'started_at', 'ended_at']]
+         .rename(columns={'ID':'to_station_id'})
+         .reset_index(drop=True))
+
+  return df2
+
+def get_rides_info(df : pd.DataFrame) -> pd.DataFrame:
+  rides_col : str = ['ride_id', 'rideable_type', 'started_at', 'ended_at', 'start_station_name', 'end_station_name']
+  return df.loc[:, rides_col]
 
 def run():
   raw_data_path = os.path.join(DATA_DIR, 'raw')
@@ -60,14 +118,25 @@ def run():
     filename = filenames[0]
     filepath = os.path.join(raw_data_year_path, filename)
     df = get_raw_data(filepath)
-    clean_df = clean_data(df)
-    print(filepath)
-    print(df.shape)
-    print(clean_df.shape)
-    break
+    return df
+    # clean_df = clean_data(df)
+    # stations = get_station_info(clean_df)
+    # rides = get_rides_info(clean_df)
+    # create_files('stations.csv',
+    #              'mapping.csv',
+    #              'rides.csv',
+    #              stations,
+    #              rides)
+    # x = pd.read_csv('stations.csv')
+    # y = pd.read_csv('mapping.csv')
+    # z = pd.read_csv('rides.csv')
+    # print(x.shape)
+    # print(y.shape)
+    # print(z.shape)
+    # break
 
-if __name__ == '__main__':
-  run()
+# if __name__ == '__main__':
+  # run()
 
 # load data - i.e read data into a pandas df
 # check data frame has the correct column names i.e check initial columns 
