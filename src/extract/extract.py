2b24107cd636a86ef7f3e278d5f833bb685b1e21
Eyob Tadele-Manhardt <eyobtade@buffalo.edu>
2023-03-03 22:05:49 -0500

    Refactor extract.py
    Removed functions from extract.py and added them to util.py.

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -1,88 +1,55 @@
-from time import time 
-from datetime import datetime
 import subprocess
 import os
-from dateutil.relativedelta import relativedelta
 import pandas as pd
 import glob
 
-# "https://divvy-tripdata.s3.amazonaws.com/202004-divvy-tripdata.zip"
-URL_PREFIX = "https://divvy-tripdata.s3.amazonaws.com"
-FILENAME = "divvy-tripdata.zip"
-
-
-def create_path(path: str) -> None:
-  if not os.path.exists(path):
-    os.makedirs(path)
-
-def format_data_url(date_str : str) -> str:
-  return f"{URL_PREFIX}/{date_str}-{FILENAME}" 
-
-def get_data_urls(start_date, end_date):
-  dates = generate_dates(start_date, end_date)
-  filenames = [format_data_url(date) for date in dates]
-  return filenames
-
-def generate_dates(start_date : datetime, end_date : datetime) -> list[str]:
-    dates = []
-    current_date = start_date
-    
-    while current_date <= end_date:
-      dates.append(current_date.strftime("%Y%m"))
-      current_date += relativedelta(months=1)
-    
-    return dates
-
-def validate_date(start_date, end_date):
-  april_2020 = datetime(year=2020, month=4, day=1) # min date
-  current_date = datetime.now() # max date 
-  return start_date >= april_2020 and end_date <= current_date
-
 def download_file_from_web(url: str, filename: str, destination: str, compressed=True) -> None:
-  try:
-    data_path = os.path.join(destination, filename)
-    # Download data
-    subprocess.run(f'curl -sSL {url} > {data_path}', shell=True, check=True)
-    if compressed:
-      # Extract zipfile content
-      subprocess.run(f'unzip -o {data_path} -d {destination}', shell=True, check=True)
-      # Remove zipfile
-      subprocess.run(f'rm {data_path}', shell=True, check=True)
-  except subprocess.CalledProcessError as e:
-    print(f"Error: {e}")
+    """Download a file from a URL and save it in a destination directory.
 
-def extract_divvy_biketrip_dataset(start_date, end_date, destination, date_format : str = "%Y-%m-%d") -> None:
-  start_date = datetime.strptime(start_date, date_format)
-  end_date = datetime.strptime(end_date, date_format)
+    Args:
+      url (str): URL to download the file.
+      filename (str): Name of the file to save.
+      destination (str): Directory where the file should be saved.
+      compressed (bool, optional): Whether the file is compressed. Defaults to True.
 
-  if validate_date(start_date, end_date):
-    urls = get_data_urls(start_date, end_date)
-    # Download data 
-    for url in urls:
-      filename = url[-25:]
-      year = filename[:4]
-      path = os.path.join(destination, year)
-      create_path(path)
-      download_file_from_web(url, filename, path)
-  else:
-    print("Invalid Date")
-
+    Returns:
+      None
+    """
+    try:
+      data_path = os.path.join(destination, filename)
+      # Download data
+      subprocess.run(f'curl -sSL {url} > {data_path}', shell=True, check=True)
+      if compressed:
+        # Extract zipfile content
+        subprocess.run(f'unzip -o {data_path} -d {destination}', shell=True, check=True)
+        # Remove zipfile
+        subprocess.run(f'rm {data_path}', shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+      print(f"Error: {e}")
 
 def combine_csv_files(input_dir: str, output_file: str) -> None:
- 
-  start_year, end_year = 2020, 2022
-  df_list = []
+    """Combine all CSV files in a directory into a single CSV file.
 
-  for year in range(start_year, end_year + 1):
-    data_dir = os.path.join(input_dir, str(year))
-    all_files = glob.glob(data_dir + "/*.csv")
-    for filename in all_files:
-      df = pd.read_csv(filename, index_col=None, header=0)
-      table_name = filename.replace(".csv", "")
-      df['table_name'] = table_name # keep track of the file
-      df_list.append(df)
-      print(f"added {filename} to df list")
+    Args:
+      input_dir (str): Directory containing the input CSV files.
+      output_file (str): Name of the output CSV file.
 
-  combined_df = pd.concat(df_list, axis=0, ignore_index=True)
-   # Write the combined data to a single CSV file
-  combined_df.to_csv(output_file, index=False)
+    Returns:
+      None
+    """
+    start_year, end_year = 2020, 2022
+    df_list = []
+
+    for year in range(start_year, end_year + 1):
+      data_dir = os.path.join(input_dir, str(year))
+      all_files = glob.glob(data_dir + "/*.csv")
+      for filename in all_files:
+        df = pd.read_csv(filename, index_col=None, header=0)
+        table_name = filename.replace(".csv", "")
+        df['table_name'] = table_name # keep track of the file
+        df_list.append(df)
+        print(f"added {filename} to df list")
+
+    combined_df = pd.concat(df_list, axis=0, ignore_index=True)
+    # Write the combined data to a single CSV file
+    combined_df.to_csv(output_file, index=False)
--- src/helpers/util.py
+++ src/helpers/util.py
@@ -1,20 +1,112 @@
 import os
-import zipfile
 from pathlib import Path
-import pandas as pd
 from datetime import datetime
 from typing import Callable, List, Tuple, Union
 import pandas as pd
 import geopandas as gpd
+from extract.extract import *
 from transform.transform import *
+from dateutil.relativedelta import relativedelta
 
 
+URL_PREFIX = "https://divvy-tripdata.s3.amazonaws.com"
+FILENAME = "divvy-tripdata.zip"
+
+## Util for extract
+def extract_divvy_biketrip_dataset(start_date, end_date, destination, date_format : str = "%Y-%m-%d") -> None:
+  """Extract the Divvy biketrip dataset for a given date range and save the files
+  in a directory.
+
+  Args:
+    start_date (str): Start date for the range in the format "YYYY-MM-DD".
+    end_date (str): End date for the range in the format "YYYY-MM-DD".
+    destination (str): Directory where the extracted files should be saved.
+    date_format (str, optional): Format of the date strings. Defaults to "%Y-%m-%d".
+
+  Returns:
+    None
+  """
+  start_date = datetime.strptime(start_date, date_format)
+  end_date = datetime.strptime(end_date, date_format)
+
+  if validate_date(start_date, end_date):
+    urls = get_data_urls(start_date, end_date)
+    # Download data 
+    for url in urls:
+      filename = url[-25:]
+      year = filename[:4]
+      path = os.path.join(destination, year)
+      create_path(path)
+      download_file_from_web(url, filename, path)
+  else:
+    print("Invalid Date")
+
+def create_path(path: str) -> None:
+  """Create directory if it does not exist.
+  """
+  if not os.path.exists(path):
+    os.makedirs(path)
+
+def format_data_url(date_str : str) -> str:
+    """Create the URL to download Divvy biketrip dataset for a given date.
+    """
+    return f"{URL_PREFIX}/{date_str}-{FILENAME}" 
+
+def get_data_urls(start_date, end_date):
+    """Generate a list of URLs for downloading Divvy biketrip dataset
+    for a range of dates.
+
+    Args:
+      start_date (datetime): Start date of the range.
+      end_date (datetime): End date of the range.
+
+    Returns:
+      List of str: List of URLs for downloading the dataset.
+    """
+    dates = generate_dates(start_date, end_date)
+    filenames = [format_data_url(date) for date in dates]
+    return filenames
+
+def generate_dates(start_date : datetime, end_date : datetime) -> list[str]:
+    """Generate a list of date strings for a range of dates.
+
+    Args:
+      start_date (datetime): Start date of the range.
+      end_date (datetime): End date of the range.
+
+    Returns:
+      list[str]: List of date strings in the format "YYYYMM".
+    """
+    dates = []
+    current_date = start_date
+    
+    while current_date <= end_date:
+      dates.append(current_date.strftime("%Y%m"))
+      current_date += relativedelta(months=1)
+    
+    return dates
+
+def validate_date(start_date, end_date):
+    """Validate if the start and end dates for the Divvy biketrip dataset
+    download are within a valid range.
+
+    Args:
+      start_date: Start date for the range.
+      end_date: End date for the range.
+
+    Returns:
+      bool: True if dates are within valid range, False otherwise.
+    """
+    april_2020 = datetime(year=2020, month=4, day=1) # min date
+    current_date = datetime.now() # max date 
+    return start_date >= april_2020 and end_date <= current_date
+
+
+## Util for transform 
 filepath_state =  os.path.join(str(Path(__file__).parents[2]), 'data', 'processed', 'cb_2018_us_state_500k.shp')
 filepath_neighborhood =  os.path.join(str(Path(__file__).parents[2]), 'data', 'processed', 'chicago_neighborhoods.geojson')
 states = gpd.read_file(filepath_state)
 neighborhood = gpd.read_file(filepath_neighborhood)
-
-
 
 def get_state(lat, lng, row, states = states):
   # print(row[lng], row[lat])
@@ -70,7 +162,6 @@ def build_station_df(df) -> pd.DataFrame:
                 .pipe(update_column_name, c_mapping_2)
          )
   return station_df
-
 
 
 if __name__ == "__main__":
