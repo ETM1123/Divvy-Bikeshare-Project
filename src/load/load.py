3be00ed087b77a4587fb097a89a5bba85eb06f32
Eyob Tadele-Manhardt <eyobtade@buffalo.edu>
2023-03-05 00:57:26 -0500

    delete extract, transform, load folder

--- src/load/load.py
+++
@@ -1,6 +0,0 @@
-import os
-import pandas as pd
-
-def send_to_csv(df : pd.DataFrame, filename : str, destination : str) -> None:
-  filepath = os.path.join(destination, filename)
-  return df.to_csv(filepath, index=False)
